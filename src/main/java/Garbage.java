a2265d4b0b9314bc015de891e4064a87f1cbbffe
Graeme O <graeme.ong>
2018-04-13 21:53:04 +1000

    [GraemeO] Update Garbage to cause a memory leak

--- src/main/java/Garbage.java
+++ src/main/java/Garbage.java
@@ -1,7 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class Garbage
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("This is garbage!");
+ 
+        for (int i=0; i<10000; i++) {
+            BufferedReader br = new BufferedReader(new FileReader("file.txt"));
+            Thread.sleep(10);
+        }
+
+        System.out.println("Putting it to sleep");
+        Thread.sleep(100000000);
     }
 }
